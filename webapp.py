84979eec7339206af39d373741943a4d2e15567e
Pedro Matumoto <pedromatumoto@gmail.com>
2025-04-27 22:19:16 -0300

    feat: audio recording and transicription saving

--- webapp.py
+++ webapp.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+from datetime import datetime
+import time
+import queue
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 import streamlit as st
+import pydub
 
 import openai
 from dotenv import find_dotenv, load_dotenv
@@ -6,13 +12,77 @@ from dotenv import find_dotenv, load_dotenv
 from transcript import transcribe_audio
 from chatting import chat
 
+DIR_FILES = Path(__file__).parent / "files"
+DIR_FILES.mkdir(exist_ok=True)
+
 _ = load_dotenv(find_dotenv())
 
 client = openai.OpenAI()
 
 
+def add_audio_chunk(audio_frame, audio_chunk):
+    for frame in audio_frame:
+        audio_chunk += pydub.AudioSegment(
+            data=frame.to_ndarray().tobytes(),
+            sample_width=frame.format.bytes,
+            frame_rate=frame.sample_rate,
+            channels=len(frame.layout.channels),
+        )
+    return audio_chunk
+
+
+def save_transcription(file_path, transcription):
+    with open(file_path, "w") as f:
+        f.write(transcription)
+
+
 def tab_recorder():
-    st.markdown("tab_recorder")
+    webrtx_ctx = webrtc_streamer(
+        key="meeting_recorder",
+        mode=WebRtcMode.SENDONLY,
+        audio_receiver_size=1024,
+        media_stream_constraints={
+            "video": False,
+            "audio": True,
+        },
+    )
+
+    if not webrtx_ctx.state.playing:
+        return
+
+    container = st.empty()
+    container.markdown("Recording...")
+    dir_meeting = DIR_FILES / datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    dir_meeting.mkdir(exist_ok=True)
+
+    last_transcription = time.time()
+    audio_chunk = pydub.AudioSegment.empty()
+    full_audio = pydub.AudioSegment.empty()
+    transcription = ""
+
+    while True:
+        if webrtx_ctx.audio_receiver:
+            try:
+                audio_frame = webrtx_ctx.audio_receiver.get_frames(timeout=1)
+            except queue.Empty:
+                time.sleep(0.1)
+                continue
+            full_audio = add_audio_chunk(audio_frame, audio_chunk)
+            audio_chunk = add_audio_chunk(audio_frame, audio_chunk)
+            if len(audio_frame) > 0:
+                full_audio.export(dir_meeting / "audio.mp3", format="mp3")
+                if time.time() - last_transcription > 10:
+                    last_transcription = time.time()
+                    audio_chunk.export(dir_meeting / "audio_chunk.mp3", format="mp3")
+                    transcription_chunk = transcribe_audio(
+                        dir_meeting / "audio_chunk.mp3"
+                    )
+                    transcription += transcription_chunk
+                    save_transcription(dir_meeting / "transcription.txt", transcription)
+                    container.markdown(transcription)
+                    audio_chunk = pydub.AudioSegment.empty()
+        else:
+            break
 
 
 def tab_summarizer():
@@ -22,7 +92,7 @@ def tab_summarizer():
 def main():
     st.set_page_config(
         page_title="Omoidasu - Meeting Synthesizer",
-        page_icon=":robot:",
+        page_icon="assets/logo_omoidasu.png",
         layout="wide",
     )
 
